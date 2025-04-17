8571105e98768663bee4349b8a795c4cb9d26b54
ripkihebat <ripkeyajah@gmail.com>
2025-04-17 15:49:46 +0700

    menuCase1sampai3

--- MainSiakad.java
+++ MainSiakad.java
@@ -23,5 +23,38 @@ public class MainSiakad {
             new Penilaian(daftarMahasiswa[2], daftarMK[1], 85, 90, 95),
             new Penilaian(daftarMahasiswa[2], daftarMK[2], 80, 90, 65)
         };
+        int pilihan;
+
+        do {
+            System.out.println("\n=== MENU SISTEM AKADEMIK ===");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan Daftar Mata Kuliah");
+            System.out.println("3. Tampilkan Data Penilaian");
+            System.out.println("4. Urutkan Mahasiswa Berdasarkan Nilai Akhir");
+            System.out.println("5. Cari Mahasiswa Berdasarkan NIM");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (pilihan) {
+                case 1:
+                    System.out.println("\nDaftar Mahasiswa:");
+                    for (Mahasiswa mhs : daftarMahasiswa) {
+                        mhs.tampilMahasiswa();
+                    }
+                    break;
+                case 2:
+                    System.out.println("\nDaftar Mata Kuliah:");
+                    for (MataKuliah mk : daftarMK) {
+                        mk.tampilMatakuliah();
+                    }
+                    break;
+                case 3:
+                    System.out.println("\nData Penilaian:");
+                    for (Penilaian p : dataPenilaian) {
+                        p.tampilPenilaian();
+                    }
+                    break;
     }
 }
